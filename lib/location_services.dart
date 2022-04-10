e7d2ae9b165e66607a927f5399dca9cedc66890d
NelsonWang <wangnelson2@gmail.com>
2022-04-09 20:23:52 -0400

    fucntional location services

--- lib/location_services.dart
+++ lib/location_services.dart
@@ -34,17 +34,19 @@ class LocationService {
     final String url =
         'https://maps.googleapis.com/maps/api/directions/json?origin=$origin&destination=$destination&key=$key';
 
+    print(url);
     var response = await http.get(Uri.parse(url));
+    print(response.body);
     var json = convert.jsonDecode(response.body);
 
     var results = {
       'bounds_me': json["routes"][0]["bounds"]["northeast"],
-      'bounds_sw': json["routes"][0]["bounds"]["southeast"],
-      'start_location': json["routes"][0]["legs"]["start_location"],
+      'bounds_sw': json["routes"][0]["bounds"]["southwest"],
+      'start_location': json["routes"][0]["legs"][0]["start_location"],
       'end_location': json["routes"][0]["legs"][0]["end_location"],
-      'polyline': json["routes"][0]["overview_polyline"]["point"],
+      'polyline': json["routes"][0]["overview_polyline"]["points"],
     };
 
-    print(json);
+    print(results);
   }
 }
